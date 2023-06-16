184e2b160798d5f3e2b9023bd1778855eaa1daab
@vt211_fyeo <vt211_fyeo@student.ztu.edu.ua>
2023-06-16 21:39:42 +0300

    v3

--- api/index.js
+++ api/index.js
@@ -1,21 +1,229 @@
-const express = require('express');
-const cors = require('cors');
+import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import { JsonWebTokenError } from 'jsonwebtoken';
+import { UserModel as User } from './models/User.js';
+import { PlaceModel as Place } from './models/Place.js';
+import { BookingModel as Booking } from './models/Booking.js';
+import cookieParser from 'cookie-parser';
+import imageDownloader from 'image-downloader';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import multer from 'multer';
+import { fstat as fs } from 'fs';
+import { dirname } from 'path';
+import mime from 'mime-types';
+import { fileURLToPath } from 'url';
+import { config } from 'dotenv';
+config();
 const app = express();
 
-app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:5173',
+const mongoURL = process.env.MONGO_URL;
+mongoose
+  .connect(mongoURL)
+  .then(async () => {
+    console.log('DB is connected!');
+  })
+  .catch((error) => console.log(error));
 
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
+const bucket = 'dawid-booking-app';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:5173',
 }));
 
-app.get('/test', (reg, res) => {
-    res.json('test ok');
+async function uploadToS3(path, originalFilename, mimetype) {
+  const client = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+  const parts = originalFilename.split('.');
+  const ext = parts[parts.length - 1];
+  const newFilename = Date.now() + '.' + ext;
+  await client.send(new PutObjectCommand({
+    Bucket: bucket,
+    Body: fs.readFileSync(path),
+    Key: newFilename,
+    ContentType: mimetype,
+    ACL: 'public-read',
+  }));
+  return `https://${bucket}.s3.amazonaws.com/${newFilename}`;
+}
+
+function getUserDataFromReq(req) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
+  });
+}
+
+app.get('/api/test', (req, res) => {
+  res.json('test ok');
 });
 
-app.post('/register', (req, res) => {
-    const {name, email, password} = req.body;
-    res.json({name, email, password});
+app.post('/api/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+
+});
+
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email: userDoc.email,
+        id: userDoc._id
+      }, jwtSecret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token).send(userDoc);
+      });
+    } else {
+      res.status(422).json('pass not ok');
+    }
+  } else {
+    res.json('not found');
+  }
+});
+
+app.get('/api/profile', (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post('/api/logout', (req, res) => {
+  res.cookie('token', '').json(true);
+});
+
+
+app.post('/api/upload-by-link', async (req, res) => {
+  const { link } = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  await imageDownloader.image({
+    url: link,
+    dest: '/tmp/' + newName,
+  });
+  const url = await uploadToS3('/tmp/' + newName, newName, mime.lookup('/tmp/' + newName));
+  res.json(url);
+});
+
+const photosMiddleware = multer({ dest: '/tmp' });
+app.post('/api/upload', photosMiddleware.array('photos', 100), async (req, res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname, mimetype } = req.files[i];
+    const url = await uploadToS3(path, originalname, mimetype);
+    uploadedFiles.push(url);
+  }
+  res.json(uploadedFiles);
+});
+
+app.post('/api/places', (req, res) => {
+  const { token } = req.cookies;
+  const {
+    title, address, addedPhotos, description, price,
+    perks, extraInfo, checkIn, checkOut, maxGuests,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner: userData.id, price,
+      title, address, photos: addedPhotos, description,
+      perks, extraInfo, checkIn, checkOut, maxGuests,
+    });
+    res.json(placeDoc);
+  });
+});
+
+app.get('/api/user-places', (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
+});
+
+app.get('/api/places/:id', async (req, res) => {
+  const { id } = req.params;
+  res.json(await Place.findById(id));
+});
+
+app.put('/api/places', async (req, res) => {
+  const { token } = req.cookies;
+  const {
+    id, title, address, addedPhotos, description,
+    perks, extraInfo, checkIn, checkOut, maxGuests, price,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.findById(id);
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+        title, address, photos: addedPhotos, description,
+        perks, extraInfo, checkIn, checkOut, maxGuests, price,
+      });
+      await placeDoc.save();
+      res.json('ok');
+    }
+  });
+});
+
+app.get('/api/places', async (req, res) => {
+  res.json(await Place.find());
+});
+
+app.post('/api/bookings', async (req, res) => {
+  const userData = await getUserDataFromReq(req);
+  const {
+    place, checkIn, checkOut, numberOfGuests, name, phone, price,
+  } = req.body;
+  Booking.create({
+    place, checkIn, checkOut, numberOfGuests, name, phone, price,
+    user: userData.id,
+  }).then((doc) => {
+    res.json(doc);
+  }).catch((err) => {
+    throw err;
+  });
+});
+
+
+
+app.get('/api/bookings', async (req, res) => {
+  const userData = await getUserDataFromReq(req);
+  res.json(await Booking.find({ user: userData.id }).populate('place'));
 });
 
 app.listen(4000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,47 +1,44 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import { useState } from "react";
+import { http } from "../../axios";
 
 export default function RegisterPage() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    function registerUser(ev) {
+    async function registerUser(ev) {
         ev.preventDefault();
-        axios.post('/register', {
-            name,
-            email,
-            password
-        }).then((res)=>{
-            console.log(res);
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+        try {
+            await http.post('/register', {
+                name,
+                email,
+                password,
+            });
+            alert('Registration successful. Now you can log in');
+        } catch (e) {
+            alert('Registration failed. Please try again later');
+        }
     }
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-                <h1 className="text-4xl text-center mb-4">Реєстрація</h1>
+                <h1 className="text-4xl text-center mb-4">Register</h1>
                 <form className="max-w-md mx-auto" onSubmit={registerUser}>
                     <input type="text"
-                        placeholder="логін"
+                        placeholder="John Doe"
                         value={name}
-                        onChange={ev => setName(ev.target.value)}
-                    />
+                        onChange={ev => setName(ev.target.value)} />
                     <input type="email"
-                        placeholder="електрона пошта"
+                        placeholder="your@email.com"
                         value={email}
-                        onChange={ev => setEmail(ev.target.value)}
-                    />
+                        onChange={ev => setEmail(ev.target.value)} />
                     <input type="password"
-                        placeholder="пароль"
+                        placeholder="password"
                         value={password}
-                        onChange={ev => setPassword(ev.target.value)}
-                    />
-                    <button className="primary">Зареєструватися</button>
+                        onChange={ev => setPassword(ev.target.value)} />
+                    <button className="primary">Register</button>
                     <div className="text-center py-2 text-gray-500">
-                        Ви зареєстрованні ? <Link className="underline text-black" to={'/login'}>Увійти</Link>
+                        Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
                     </div>
                 </form>
             </div>
